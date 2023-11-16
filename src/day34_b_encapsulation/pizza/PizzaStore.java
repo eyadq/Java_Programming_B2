948057e82910cff23c0f7f2c50622b5e4dd957a2
Eyad Quraan <EyadQuraan@gmail.com>
2023-11-15 22:04:47 -0500

    static recap, encapsulation, inheritance intro

---
+++ src/day34_b_encapsulation/pizza/PizzaStore.java
@@ -0,0 +1,28 @@
+package day34_b_encapsulation.pizza;
+
+import day34_b_encapsulation.pizza.Pizza;
+
+public class PizzaStore {
+    public static void main(String[] args) {
+
+        Pizza pizzaOne = new Pizza("Tall", -3);
+
+        //System.out.println(pizzaOne.size);
+        //System.out.println(pizzaOne.numberOfToppings);
+
+        System.out.println(pizzaOne.getSize());
+        System.out.println(pizzaOne.getNumberOfToppings());
+
+
+
+        Pizza pizzaTwo = new Pizza("small", 3);
+        System.out.println(pizzaTwo.getSize());
+        System.out.println(pizzaTwo.getNumberOfToppings());
+
+        pizzaTwo.calculatePrice();
+        System.out.println(pizzaTwo);
+
+
+
+    }
+}
